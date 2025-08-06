1e61d153664d6db57bbda9f091e7325dd65f74df
Zadeis <tyler.bibeault@pm.me>
2025-08-06 18:23:45 -0400

    Further refactor case builder.
    Feat: Add ManualSelection to BuildStep enum for use in case builder.
    Feat: Added combined selection_screen function logic to be reused for each step and removed rom specific selection function.
    Feat: Refactor run_case_builder to account for new function.

--- crates/game_case_creator/src/cli_structs.rs
+++ crates/game_case_creator/src/cli_structs.rs
@@ -24,7 +24,7 @@ impl Default for AppConfig {
 pub enum BuildStep {
     #[default]
     RomSelection,
-    //ManualSelection,
+    ManualSelection,
     //RomhackSelection,
     //ImageSelection,
     //VideoSelection,
--- crates/game_case_creator/src/modes/case_build.rs
+++ crates/game_case_creator/src/modes/case_build.rs
@@ -17,66 +17,96 @@ pub fn run_case_builder(
 ) {
     let current_step = siv
         .user_data::<AppState>()
-        .unwrap().build_state.current_step.clone();
+        .unwrap()
+        .build_state
+        .current_step
+        .clone();
 
     match current_step {
-        BuildStep::RomSelection => show_rom_selection_screen(siv),
+        BuildStep::RomSelection => show_selection_screen(
+            siv,
+            current_step,
+            "Step 1: Add ROMs or an archive file",
+            "Select a ROM file or a directory".to_string(),
+        ),
+        BuildStep::ManualSelection => show_selection_screen(
+            siv,
+            current_step,
+            "Step 2: Add manuals",
+            "Select a manual file".to_string(),
+        ),
     }
 
 }
 
-fn show_rom_selection_screen(siv: &mut Cursive) {
-    let (staged_roms, start_path) = siv
+fn show_selection_screen(
+    siv: &mut Cursive,
+    step: BuildStep,
+    title: &str,
+    task_text: String,
+) {
+    let (staged_files, start_path) = siv
         .with_user_data(|app_state: &mut AppState| {
-            (
-                app_state.build_state.staged_roms.clone(),
-                app_state.config.default_browse_directory.clone(),
-            )
+            let staged_files = match step {
+                BuildStep::RomSelection => app_state.build_state.staged_roms.clone(),
+                BuildStep::ManualSelection => app_state.build_state.staged_manuals.clone(),
+            };
+            (staged_files, app_state.config.default_browse_directory.clone())
         })
-        .unwrap();
+        .expect("Could not get AppState");
+    let on_next_step = step.clone();
 
     let on_add = move |s: &mut Cursive| {
         let cb_sink = s.cb_sink().clone();
+        let current_step_clone = step.clone();
 
         let on_selection_callback = move |selected_paths: Option<Vec<PathBuf>>| {
             if let Some(paths) = selected_paths {
                 cb_sink
                     .send(Box::new(move |siv: &mut Cursive| {
                         siv.with_user_data(|app_state: &mut AppState| {
-                            app_state.build_state.staged_roms.extend(paths);
+                            let staged_files_mut = match current_step_clone {
+                                BuildStep::RomSelection => &mut app_state.build_state.staged_roms,
+                                BuildStep::ManualSelection => &mut app_state.build_state.staged_manuals,
+                            };
+                            staged_files_mut.extend(paths);
                         })
-                        .unwrap();
-                        
+                        .expect("Could not get AppState");
+
                         siv.pop_layer();
-                        show_rom_selection_screen(siv);
+                        run_case_builder(siv);
                     }))
-                    .unwrap();
+                    .expect("Could not send callback to UI thread");
             }
         };
 
         file_and_directory_selector(
-            s.cb_sink().clone(), 
-            start_path.clone(), 
-            "Select a ROM file or a directory".to_string(), 
-            false, 
-            on_selection_callback
+            s.cb_sink().clone(),
+            start_path.clone(),
+            task_text.to_string(),
+            false,
+            on_selection_callback,
         );
     };
 
-    let on_next = |s: &mut Cursive| {
+    let on_next = move |s: &mut Cursive| {
         s.with_user_data(|app_state: &mut AppState| {
-            // We just set the state back to where we want to be
-            app_state.build_state.current_step = BuildStep::RomSelection;
-        }).unwrap();
-        
+            app_state.build_state.current_step = match on_next_step.clone() {
+                BuildStep::RomSelection => BuildStep::ManualSelection,
+                BuildStep::ManualSelection => BuildStep::ManualSelection,
+            };
+        })
+        .expect("Could not get AppState");
+
         s.pop_layer();
         run_case_builder(s);
     };
 
     show_build_screen(
-        siv, 
-        "Step 1: Add ROMs or an archive file".to_string(), 
-        &staged_roms, 
-        on_add, 
-        on_next);
+        siv,
+        title.to_string(),
+        &staged_files,
+        on_add,
+        on_next,
+    );
 }
